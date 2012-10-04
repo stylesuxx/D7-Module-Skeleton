34fa78437a9d097352b39ed95c405fd03a1fad4c
Chris <chris@1337.af>
2012-10-05 00:03:56 +0200

    Added check if destination is writable. Added possibility to provide destination dir

--- generate.sh
+++ generate.sh
@@ -2,24 +2,36 @@
 
 # This script generates a new skeleton module for Drupal 7.x
 USAGE='Usage: ./generate.sh "Module Name"' 
-if [ $# -lt 1 ] || [ $# -gt 1 ]; then
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
 	echo $USAGE
 	exit
 fi
 
 MODULENAME=$1
+DESTINATION=`pwd`
 MACHINENAME=`echo "${MODULENAME,,}" | sed s/\ /_/g`
+
+# Set the destination if given
+if [ $# -eq 2 ];then
+	DESTINATION=$2
+fi
+
+# Check if destination is writable
+if [ ! -w $DESTINATION ]; then
+	echo "Destination directory is not writable."
+	exit
+fi
 
 # Check if all required files exist
 if [ ! -d module ] || [ ! -e module/module.info.tpl ] || [ ! -e module/module.install.tpl ] || [ ! -e module/module.module.tpl ]; then
 	echo "There are some files missing - will not create new module."
-	exit 1
+	exit
 fi
 
 # Check if there alredy is a module with this name
-if [ -e $MACHINENAME ]; then
+if [ -e $DESTINATION/$MACHINENAME ]; then
 	echo "A Module with this name already exists - will not create new module."
-	exit 2
+	exit
 fi
 
 # Generate the Module
@@ -27,9 +39,9 @@ echo -e "Generating new Module..."
 echo -e "Machinename: $MACHINENAME"
 
 # Replace the placeholders in the files
-mkdir $MACHINENAME
-sed "s/@MODULENAME/$MODULENAME/g" module/module.info.tpl > $MACHINENAME/$MACHINENAME.info
-sed "s/@MACHINENAME/$MACHINENAME/g" module/module.install.tpl > $MACHINENAME/$MACHINENAME.install
-sed "s/@MACHINENAME/$MACHINENAME/g" module/module.module.tpl > $MACHINENAME/$MACHINENAME.module
+mkdir $DESTINATION/$MACHINENAME
+sed "s/@MODULENAME/$MODULENAME/g" module/module.info.tpl > $DESTINATION/$MACHINENAME/$MACHINENAME.info
+sed "s/@MACHINENAME/$MACHINENAME/g" module/module.install.tpl > $DESTINATION/$MACHINENAME/$MACHINENAME.install
+sed "s/@MACHINENAME/$MACHINENAME/g" module/module.module.tpl > $DESTINATION/$MACHINENAME/$MACHINENAME.module
 
 echo -e "Module successfully created."
